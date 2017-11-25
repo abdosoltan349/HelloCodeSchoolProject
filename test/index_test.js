e773097ae200eaf0a83d2bab5249702752b9db75
abdosoltan349 <33980086+abdosoltan349@users.noreply.github.com>
2017-11-25 17:07:05 +0200

    Update index_test.js

--- test/index_test.js
+++ test/index_test.js
@@ -1,37 +1,38 @@
 var jsdom = require('jsdom'),
     fs = require('fs'),
-    assert = require('chai').assert,
-    file = fs.readFileSync('index.html').toString();
-
-describe('Your HTML Page', function() {
-  var window;
-  before(function(next) {
-    jsdom.env(
-      file,
-      ["http://code.jquery.com/jquery.js"],
-      function (err, w) {
-        if(err) { next(err); }
-        window = w;
-        next();
-      }
-    );
-  });
-
-  it('should have a title @title', function() {
-    assert.equal(window.$('title').length, 1, 'Make sure to create a `title` element.');
-  });
-
-  it('should have a title that contains your name @title', function() {
-    assert.notEqual(window.$('title').text(), '', 'Make sure to set the content of the `title` element to your Code School username.');
-  });
-
-  it('should have a h1 element @h1', function() {
-    assert.isAtLeast(window.$('h1').length, 1, "Make sure to create an `h1` element.");
-  });
-
-  it('should have content in the h1 element @h1', function() {
-    assert.equal(window.$('h1').text(), 'Hello, Code School!', "Make sure to set the content of your `h1` element to 'Hello, Code School!'.");
-  });
+		    assert = require('chai').assert,
+		    file = fs.readFileSync('index.html').toString();
+		
+		describe('Your HTML Page', function() {
+		  var window;
+		  before(function(next) {
+		    jsdom.env(
+		      file,
+		      ["http://code.jquery.com/jquery.js"],
+		      function (err, w) {
+		        if(err) { next(err); }
+		        window = w;
+		        next();
+		      }
+		    );
+		  });
+		
+		  it('should have a different title', function() {
+		    assert.notEqual(window.$('title').text(), 'Hello Code School');
+		  });
+		
+		  it('should have a different h1', function() {
+		    assert.notEqual(window.$('h1').length, 0);
+		  });
+		
+		  it('should have a ul', function() {
+		    assert.notEqual(window.$('ul').length, 0);
+		  });
+		
+		  it('should have at least 2 li elements', function() {
+		    assert.isAtLeast(window.$('li').length, 2);
+		  });
+		});
 
   it('should have a ul @ul', function() {
     assert.isAtLeast(window.$('ul').length, 1, "Make sure to create a `ul` element.");
